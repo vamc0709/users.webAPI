390daf830d05c37dd5dd6b9c15c92749ef54f0a0
vamsi <sangeethavamsi1999@gmail.com>
2023-11-03 12:26:03 +0530

    initial commit

---
+++ Controllers/UserController.cs
@@ -0,0 +1,10 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace users.webAPI.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class WeatherForecastController : ControllerBase
+{
+    
+}
